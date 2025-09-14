43be4fd8a1ffab69323e86d8dad28fae2def006a
Trinity Washington <Trinity.Washington@bsu.edu>
2025-09-14 12:41:56 -0400

    Initial commit: add CPU scheduling project files

---
+++ RR.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * TODO: implement the RR (Round Robin) scheduling algorithm
+ */
+public class RR extends Algorithm{
+
+    public RR(List<Process> allProcesses){
+        super(allProcesses);
+    }
+
+    @Override
+    public void schedule(){}
+}
